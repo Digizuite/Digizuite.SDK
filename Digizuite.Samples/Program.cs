90e4725c23c73cef550dafbd5504f6e3791276b9
nek-digizuite <nek@digizuite.com>
2020-02-05 15:47:11 +0100

    Working on Sample

---
+++ Digizuite.Samples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Digizuite.Models;
+
+namespace Digizuite.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+
+
+        static void Test1()
+        {
+            var config = new Configuration()
+            {
+                AccessKeyDuration = new TimeSpan(86400000L),
+                BaseUrl = "https://dam.dev.digizuite.com/",
+                SystemUsername = "",
+                SystemPassword = ""
+            };
+            var httpClient = new HttpClientFactory(config);
+            
+            var auth = new DamAuthenticationService(config, httpClient, null);
+
+            
+
+        }
+    }
+}
---
+++ Digizuite/HttpClientFactory.cs
@@ -0,0 +1,20 @@
+﻿using Digizuite.Models;
+using RestSharp;
+
+namespace Digizuite
+{
+    public class HttpClientFactory : IHttpClientFactory
+    {
+        private readonly Configuration _configuration;
+
+        public HttpClientFactory(Configuration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IRestClient GetRestClient()
+        {
+            return new RestClient(_configuration.BaseUrl);
+        }
+    }
+}
