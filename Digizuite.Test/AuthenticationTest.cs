8591f0e2dc55e2ed867c089b2e091f6dffcdaa96
nek-digizuite <nek@digizuite.com>
2020-02-06 12:39:32 +0100

    Added Tests of AuthenticationService

--- Digizuite.Test/AuthenticationTest.cs
+++ Digizuite.Test/AuthenticationTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
@@ -12,10 +13,24 @@ namespace Digizuite.Test
         {
 
             var service = ServiceProvider.GetRequiredService<IDamAuthenticationService>();
-
             var ak = await service.GetAccessKey();
-            
             Assert.That(ak, Is.Not.Empty);
+        }
+        [Test]
+        public void CanGetAccessKey_WrongPassword()
+        {
+            var service = ServiceProvider.GetRequiredService<IDamAuthenticationService>();
+            Configuration.SystemPassword = new string('0', 36);
+            var exceptionThrown = Assert.Throws<Exception>(() => service.GetAccessKey().GetAwaiter().GetResult());
+            Assert.AreEqual("Request was unsuccessful", exceptionThrown.Message);
+        }
+
+        [Test]
+        public async Task GetMemberId()
+        {
+            var service = ServiceProvider.GetRequiredService<IDamAuthenticationService>();
+            var memberId = await service.GetMemberId();
+            Assert.AreEqual(23, memberId);
         }
     }
 }
