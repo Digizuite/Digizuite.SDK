0bb9d71c0359d3e31752d7ca4357234e8793b0df
nek-digizuite <nek@digizuite.com>
2020-02-07 13:20:11 +0100

    Added UnitTest of Paging Logic

---
+++ Digizuite.Test/UnitTests/UnitTestBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Digizuite.Models;
+using Digizuite.Samples;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NUnit.Framework;
+
+namespace Digizuite.Test.UnitTests
+{
+    public abstract class UnitTestBase
+    {
+        protected Configuration Configuration;
+
+        protected IServiceProvider ServiceProvider;
+
+        protected virtual void SetupDependencies(IServiceCollection services)
+        {
+//            services.Replace(new ServiceDescriptor(typeof(IHttpClientFactory), typeof(UnitTestHttpClientFactory), ServiceLifetime.Singleton));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            var apiUrl = "https://unittest-dam.example.local";
+            var username = "fictionaluser";
+            var password = "fictionalpassword";
+            var accessKeyDuration = new TimeSpan(2, 0, 0);
+
+            if (string.IsNullOrWhiteSpace(apiUrl)) throw new ArgumentException("apiUrl was not set");
+
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentException("username was not set");
+
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("password was not set");
+
+            Configuration = new Configuration
+            {
+                BaseUrl = apiUrl,
+                SystemUsername = username,
+                SystemPassword = password,
+                AccessKeyDuration = accessKeyDuration
+            };
+
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(Configuration);
+            serviceCollection.AddDigizuite();
+            serviceCollection.AddSingleton(typeof(ILogger<>), typeof(ConsoleLogger<>));
+            SetupDependencies(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+    }
+}
