a7ccbaecab59462dea117b5bf8af0cf4694e8055
zlepper <hansen13579@gmail.com>
2020-02-10 08:37:53 +0100

    Slightly nicer api

--- Digizuite/IUploadService.cs
+++ Digizuite/IUploadService.cs
@@ -5,8 +5,52 @@ namespace Digizuite
 {
     public interface IUploadService
     {
-        Task<int> Upload(Stream stream, string filename, string computerName, IUploadProgressListener listener);
+        /// <summary>
+        /// Uploads a new asset
+        /// </summary>
+        /// <param name="stream">A stream to read the asset content from</param>
+        /// <param name="filename">The name of the file to upload</param>
+        /// <param name="computerName">Should be the name of the service uploading this file, e.g., "Digizuite Media Manager"</param>
+        /// <param name="listener">An optional listener for hooking in as the upload moves along. Do any custom operations you might have to do here</param>
+        /// <returns>The itemId of the newly uploaded asset</returns>
+        Task<int> Upload(Stream stream, string filename, string computerName, IUploadProgressListener listener = null);
+        
+        /// <summary>
+        /// Replaces an existing asset
+        /// </summary>
+        /// <param name="stream">A stream to read the asset content from</param>
+        /// <param name="filename">The name of the file to upload</param>
+        /// <param name="computerName">Should be the name of the service uploading this file, e.g., "Digizuite Media Manager"</param>
+        /// <param name="targetAssetId">The asset id of the asset to replace</param>
+        /// <param name="keepMetadata">If metadata should be kept or replace</param>
+        /// <param name="overwrite">How the asset history should be considered</param>
+        /// <param name="listener">An optional listener for hooking in as the upload moves along. Do any custom operations you might have to do here</param>
+        /// <returns>The itemId of the newly uploaded asset</returns>
         Task<int> Replace(Stream stream, string filename, string computerName, int targetAssetId,
-            bool keepMetadata, bool overwrite, IUploadProgressListener listener);
+            KeepMetadata keepMetadata, Overwrite overwrite, IUploadProgressListener listener = null);
+    }
+
+    public enum KeepMetadata
+    {
+        /// <summary>
+        /// Indicates that existing metadata on the asset should be kept
+        /// </summary>
+        Keep,
+        /// <summary>
+        /// Indicates that the metadata on the asset should be reset as if the upload was a completely new asset
+        /// </summary>
+        Reset
+    }
+
+    public enum Overwrite
+    {
+        /// <summary>
+        /// The new file will appear as a new entry in the asset history
+        /// </summary>
+        AddHistoryEntry,
+        /// <summary>
+        /// The last entry in the asset history will be overwritten with this entry
+        /// </summary>
+        ReplaceHistoryEntry
     }
 }
