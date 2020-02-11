a80191828e44f59805f92ec181ccab2116e1d1d7
nek-digizuite <nek@digizuite.com>
2020-02-11 08:46:03 +0100

    Throw better exception, when authentication fails

---
+++ Digizuite/Exceptions/AuthenticationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digizuite.Exceptions
+{
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException()
+        {
+        }
+
+        public AuthenticationException(string message) : base(message)
+        {
+        }
+
+        public AuthenticationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
