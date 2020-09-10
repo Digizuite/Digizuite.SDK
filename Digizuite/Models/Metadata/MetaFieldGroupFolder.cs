08578a0a8930fdc3d696d26d6ad8dd14d2440472
Zlepper <hansen13579@gmail.com>
2020-09-10 08:04:45 +0200

    Move MetaFieldGroupFolder to its own file

---
+++ Digizuite/Models/Metadata/MetaFieldGroupFolder.cs
@@ -0,0 +1,9 @@
+﻿namespace Digizuite.Models.Metadata
+{
+    public class MetaFieldGroupFolder
+    {
+        public int FolderId { get; set; }
+        public string Name { get; set; }
+        public int ParentFolderId { get; set; }
+    }
+}
