cc7d040ad8e39150d9ddb005a740bd4fc0514418
nek-digizuite <nek@digizuite.com>
2020-09-23 20:19:51 +0200

    DAM-4597 Git missed this file

---
+++ Digizuite/Exceptions/ProductVersionNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Digizuite.Exceptions
+{
+    public class ProductVersionNotFoundException : Exception
+    {
+        public ProductVersionNotFoundException()
+        {
+        }
+
+        public ProductVersionNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ProductVersionNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
