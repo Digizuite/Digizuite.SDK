fe09543a592d4d6fd1dfe7086c7cb07aac0694bf
Magnus Frank <mdf@DigiDom.dk>
2021-09-08 15:19:39 +0200

    init docs

---
+++ Digizuite.Samples/Initialization/Initialization.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Digizuite.Logging;
+using Digizuite.Models;
+using Digizuite.Samples.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Digizuite.Samples.Initialization
+{
+    public class Initialization
+    {
+        private static readonly ServiceProvider _serviceProvider;
+        
+        static Initialization()
+        {
+            var serviceCollection = new ServiceCollection(); 
+            
+            var config = new DigizuiteConfiguration()
+            {
+                BaseUrl = new Uri("https://<Digizuite url>.com/"),
+                SystemUsername = "<Username>",
+                SystemPassword = "<Password>"
+            };
+            
+            serviceCollection.AddDigizuite(config);
+            serviceCollection.AddSingleton(typeof(ILogger<>), typeof(SimpleLogger<>));
+            _serviceProvider = serviceCollection.BuildServiceProvider(true);
+        }
+
+        public static ServiceProvider GetServiceProvider()
+        {
+            return _serviceProvider;
+        }
+    }
+}
---
+++ Digizuite.Samples/Logging/SimpleLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Digizuite.Logging;
+using Newtonsoft.Json;
+
+namespace Digizuite.Samples.Logging
+{
+    public class SimpleLogger<T> : ILogger<T>
+    {
+        public virtual bool IsLogLevelEnabled(LogLevel level)
+        {
+            return true;
+        }
+
+        public void Log(LogLevel level, Exception exception, string message, params object[] args)
+        {
+            if (!IsLogLevelEnabled(level))
+            {
+                return;
+            }
+
+            var logLine = new Dictionary<string, object>
+            {
+                {"level", level.ToString()},
+                {"message", message},
+                {"args", args},
+            };
+
+            if (exception != null)
+            {
+                logLine["exception"] = exception;
+            }
+            
+            Console.WriteLine(JsonConvert.SerializeObject(logLine));
+        }
+    }
+}
