ae3a92ac90146647ccb7959ef1188a706830c73d
Oleg Plytnyk <oplitnik@ukad-group.com>
2021-09-14 14:01:38 +0300

    refetred non nullable

--- Digizuite/Metadata/ResponseModels/MetaFields/MasterItemReferenceMetaFieldResponse.cs
+++ Digizuite/Metadata/ResponseModels/MetaFields/MasterItemReferenceMetaFieldResponse.cs
@@ -8,7 +8,7 @@ namespace Digizuite.Metadata.ResponseModels.MetaFields
         public override MetaFieldDataType Type => MetaFieldDataType.MasterItemReference;
         public ItemType ItemType { get; set; }
         public int MaxCount { get; set; }
-        public int? RelatedMetaFieldLabelId { get; set; }
+        public int RelatedMetaFieldLabelId { get; set; }
         public int? RelatedMetaFieldId { get; set; }
     }
 }
