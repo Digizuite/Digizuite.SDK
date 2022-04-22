dd9958d260aea808cd0ff04e5c40d4919ac29093
Oleg Plytnyk <oplitnik@gmail.com>
2022-04-22 14:42:43 +0300

    Update Digizuite/Metadata/MetaFieldService.cs
    Co-authored-by: Rasmus Hansen <rhdh@digizuite.com>

--- Digizuite/Metadata/MetaFieldService.cs
+++ Digizuite/Metadata/MetaFieldService.cs
@@ -28,57 +28,14 @@ namespace Digizuite.Metadata
 
         private MetaFieldResponse CopyMetaFieldResponse(MetaFieldResponse metaFieldResponse, MetaFieldLabelResponse label)
         {
-            MetaFieldResponse field = metaFieldResponse switch
+            return metaFieldResponse with
             {
-                IntMetaFieldResponse => new IntMetaFieldResponse(),
-                StringMetaFieldResponse stringMetaFieldResponse => new StringMetaFieldResponse { MaxLength = stringMetaFieldResponse.MaxLength },
-                BitMetaFieldResponse => new BitMetaFieldResponse(),
-                NoteMetaFieldResponse noteMetaFieldResponse => new NoteMetaFieldResponse { ShowRichTextEditor = noteMetaFieldResponse.ShowRichTextEditor },
-                DateTimeMetaFieldResponse dateTimeMetaFieldResponse => new DateTimeMetaFieldResponse { ViewType = dateTimeMetaFieldResponse.ViewType},
-                MultiComboValueMetaFieldResponse  => new MultiComboValueMetaFieldResponse(),
-                ComboValueMetaFieldResponse comboValueMetaFieldResponse => new ComboValueMetaFieldResponse { ViewType = comboValueMetaFieldResponse.ViewType},
-                MasterItemReferenceMetaFieldResponse masterItemReferenceMetaFieldResponse =>
-                    new MasterItemReferenceMetaFieldResponse 
-                    { 
-                        ItemType = masterItemReferenceMetaFieldResponse.ItemType,
-                        MaxCount = masterItemReferenceMetaFieldResponse.MaxCount,
-                        RelatedMetaFieldLabelId = masterItemReferenceMetaFieldResponse.RelatedMetaFieldLabelId
-                    },
-                SlaveItemReferenceMetaFieldResponse slaveItemReferenceMetaFieldResponse => 
-                    new SlaveItemReferenceMetaFieldResponse
-                    {
-                        ItemType = slaveItemReferenceMetaFieldResponse.ItemType,
-                        RelatedMetaFieldLabelId = slaveItemReferenceMetaFieldResponse.RelatedMetaFieldLabelId
-                    },
-                FloatMetaFieldResponse => new FloatMetaFieldResponse(),
-                LinkMetaFieldResponse => new LinkMetaFieldResponse(),
-                TreeMetaFieldResponse treeMetaFieldResponse => new TreeMetaFieldResponse { RecursiveToRoot = treeMetaFieldResponse.RecursiveToRoot },
-                EditMultiComboValueMetaFieldResponse => new EditMultiComboValueMetaFieldResponse(),
-                EditComboValueMetaFieldResponse => new EditComboValueMetaFieldResponse(),
-                _ => throw new ArgumentOutOfRangeException(nameof(metaFieldResponse),
-                        $"Unknown metafield value type. Got {metaFieldResponse.GetType()}")
-            };
-
-            field.MetafieldId = metaFieldResponse.MetafieldId;
-            field.SortIndex = metaFieldResponse.SortIndex;
-            field.Required = metaFieldResponse.Required;
-            field.Readonly = metaFieldResponse.Readonly;
-            field.AutoTranslated = metaFieldResponse.AutoTranslated;
-            field.AutoTranslatedOverwriteExisting = metaFieldResponse.AutoTranslatedOverwriteExisting;
-            field.VisibilityMetaFieldId = metaFieldResponse.VisibilityMetaFieldId;
-            field.VisibilityRegex = metaFieldResponse.VisibilityRegex;
-            field.GroupId = metaFieldResponse.GroupId;
-            field.Guid = metaFieldResponse.Guid;
-            field.ItemId = metaFieldResponse.ItemId;
-            field.RestrictToItemType = metaFieldResponse.RestrictToItemType;
-            field.Audited = metaFieldResponse.Audited;
 #pragma warning disable 612
-            field.LabelId = label.LabelId;
-            field.LanguageId = label.LanguageId;
-            field.Label = label.Label;
+                LabelId = label.LabelId,
+                LanguageId = label.LanguageId,
+                Label = label.Label,
 #pragma warning restore 612
-
-            return field;
+            };
         }
 
         public async Task<List<MetaFieldResponse>> GetAllMetaFields(string? accessKey = null,
